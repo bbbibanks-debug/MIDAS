d0abb61a8dd36bfd35011a8407654f8c4088bc8d
bbbibanks-debug <bbbi.banks@gmail.com>
2026-05-22 23:05:16 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://bbbibanks-debug.github.io/MIDAS/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
