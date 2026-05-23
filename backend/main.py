649875acb5422d1e995d6263b604d34b6c221580
bbbibanks-debug <bbbi.banks@gmail.com>
2026-05-22 21:07:42 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
     return {"message": "MIDAS ONLINE"}
+
+@app.post("/upload")
+async def upload_excel(file: UploadFile = File(...)):
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    df = pd.read_excel(file_path)
+
+    preview = df.head(10).to_dict(orient="records")
+
+    return {
+        "filename": file.filename,
+        "columns": list(df.columns),
+        "rows_preview": preview,
+        "total_rows": len(df)
+    }
