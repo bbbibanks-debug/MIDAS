9d8b25223348ba72fdc849a5453794a701470787
bbbibanks-debug <bbbi.banks@gmail.com>
2026-05-22 23:03:59 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+
 import pandas as pd
 import os
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bbbibanks-debug.github.io/MIDAS/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
